3025b456683c0407d841a0e0877109f018635cd1
Catherine Loesch <catherine.loesch@protonmail.com>
2021-05-03 02:13:27 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400){
+    return 'This one is on me!'
+  } else if (feet > 2500){
+    return 'No can do.'
+  } else if (feet > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let message = city==="NYC" ? 'Ok, sounds good.' : 'No go.'
+  return message
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch(tip) {
+    case "generous":
+      message = 'Thank you so much.';
+      break;
+    case "not as generous":
+      message = "Thank you.";
+      break;
+    default:
+      message = "Bye.";
+      break;
+  }
+  return message
 }
